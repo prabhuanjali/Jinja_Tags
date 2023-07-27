47e428c8345bf3dda00f3de16220bef26070e29f
prabhuanjali <prabhuanjali2000@gmail.com>
2023-07-27 11:56:37 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def data_render(request):
+    d = {'name':'Anjali', 'age':'22'}
+    return render(request,'data_render.html', context = d)
