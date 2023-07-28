60614545843970f527010ba87bcfadc03536aa88
prabhuanjali <prabhuanjali2000@gmail.com>
2023-07-28 12:13:01 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -10,3 +10,7 @@ def data_render(request):
 def if_else(request):
     d = {'a': 10, 'b': 25}
     return render(request,'if_else.html', context = d)
+
+def if_elif_else(request):
+    d = {'a': 10, 'b': 25, 'c': 190}
+    return render(request, 'if_elif_else.html', context = d)
